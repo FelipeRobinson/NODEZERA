17980cbf4249b7e8c027f05cce99bf598672e0c7
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-08-19 20:48:16 -0300

    U´date

--- src/config/express.js
+++ src/config/express.js
@@ -1,13 +1,16 @@
 const express = require("express");
 
+
 //criando a aplicação
 const aplicacao = express();
 const rotas = require("../app/ROTAS/rotas");
 rotas(aplicacao);
 
+
 // indica o caminho para os ARQUIVOS
-aplicacao.use(express.static("arquivos"));
+aplicacao.use(express.static("files"));
 aplicacao.set("views","./views");
 aplicacao.set("view engine","ejs");
+
 
 module.exports = aplicacao;
