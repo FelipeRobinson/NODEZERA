18dd6affb6ed1cf28c52c10efa66617061485fa3
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-08-26 20:43:06 -0300

    UPDATE

--- src/app/ROTAS/rotas.js
+++ src/app/ROTAS/rotas.js
@@ -1,23 +1,7 @@
 module.exports = (aplicacao) => {
     // as rotas da aplicação
     aplicacao.get("/", (req, res) => {
-        console.log("A rota foi acessada")
-        res.send("<h1>Você obteve o molho...</h1>")
-    });
-
-    aplicacao.get("/home", (req, res) => {
-        console.log("A rota HOME foi acessada")
-        // res.send("<h1>Você obteve o molho + casa...</h1>")
-        res.render("home");
-    });
-
-    aplicacao.get("/contato", (req, res) => {
-        console.log("A rota CONTATO foi acessada ")
-        res.send("<h1>Você obteve o molho + contato...</h1>")
-    });
-
-    aplicacao.get("/pqp", (req, res) => {
-        console.log("A rota PQP foi acessada")
-        res.send("<h1>EU NÃOP AGUENTO MAISSSSSSSSSSSSSSSSSSSSSSSSSSSSSS...</h1>")
+        res.render('../views/ejs/listagemClientes', { clientes: resultados });
+        console.log("A rota foi acessada");
     });
 };
---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection((
+    host: 'regulus.cotuca.unicamp.br';
+    user: 'BD24464';
+    password: 'BD24464';
+    database: 'BD24464'
+));
+
+connection.connect(function(erro){
+    if(erro)
+        console.log("erro na conexão!");
+    else
+        console.log("conexão bem sucedida!");
+});
+
+module.exports = connection;
